b0f820b366f528161be5e8c9c154f2efbd30674c
memerso1 <memerso1@maine.rr.com>
2010-01-21 13:14:23 -0500

    test

---
+++ Render/ActionElement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rdl.Render
+{
+    public class ActionElement : Element
+    {
+        public class ActionParameter
+        {
+            public string Name = string.Empty;
+            public object Value = null;
+
+            public ActionParameter(string name, object value)
+            {
+                Name = name;
+                Value = value;
+            }
+        }
+
+        private string _hyperLink;
+        private string _bookmarkLink;
+        private string _drillThroughReportName;
+        private List<ActionParameter> _drillThroughParameterList = new List<ActionParameter>();
+
+        public ActionElement(Container parent, 
+            Rdl.Engine.ReportElement reportItem, 
+            BoxStyle style
+            )
+            : base(parent, reportItem, style)
+        {
+        }
+
+        public ActionElement(Element e)
+            : base(e)
+        {
+        }
+
+        public string BookmarkLink
+        {
+            get { return _bookmarkLink; }
+            set { _bookmarkLink = value; }
+        }
+
+        public string Hyperlink
+        {
+            get { return _hyperLink; }
+            set { _hyperLink = value; }
+        }
+
+        public string DrillThroughReportName
+        {
+            get { return _drillThroughReportName; }
+            set { _drillThroughReportName = value; }
+        }
+
+        public List<ActionParameter> DrillThroughParameterList
+        {
+            get { return _drillThroughParameterList; }
+        }
+    }
+}
